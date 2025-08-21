35b882b2b5e87c563e99bca24077e893cba4a94d
zcrouse <zcrouse@cs.washington.edu>
2025-08-20 23:57:42 -0700

    feat: Added permission list to user schema

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,8 +1,25 @@
 const mongoose = require('mongoose');
 
+// Schema type for permissions of users.
+// Includes a limiter which defines how broadly the permission can be used.
+// Some combinations of limiters and perm type will not be used,
+// for example change_perm type with location limiter.
+const permissionSchema = new mongoose.Schema({
+  type: { 
+    type: String,
+    enum: ['view_survey', 'delete_survey', 'change_perms', 'edit_profile', 'approve_user'],
+    required: true 
+  },
+  limiter: { 
+    type: String,
+    enum: ['Self', 'Location', 'All'],
+    required: true
+  }
+});
+
 // User schema definition
 // This schema defines the structure of the user data in the MongoDB database.
-// It includes fields for employee ID, first name, last name, email, phone number,
+// It includes a role along with a list of permissions each user has.
 const userSchema = new mongoose.Schema({
   employeeId: {
     type: String,
@@ -15,10 +32,19 @@ const userSchema = new mongoose.Schema({
   lastName:   { type: String, required: true },
   email:      { type: String, required: true, unique: true },
   phone:      { type: String, required: true },
+  location:   { type: String, required: true },
   role: {
     type: String,
     enum: ['Volunteer', 'Manager', 'Admin'],
     required: true
+  },
+  // List of permissions the user has, each permission must be unique by type.
+  // This means there cannot be two 'view_survey' permission types for one user.
+  // However this is not enforced by the schema by just by convention. We could
+  // add a custom validator to enforce this if needed.
+  permissions: {
+    type: [permissionSchema],
+    alias: 'perms'
   },
   approvalStatus: {
     type: String,
