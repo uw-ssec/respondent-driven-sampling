77e93651d8fda8437084aa32396ca52a62170455
Zack Crouse <zacktcrouse@gmail.com>
2025-09-02 22:20:26 -0700

    feat: Created messages file to hold common http return messages accross files.

---
+++ server/messages.js
@@ -0,0 +1,11 @@
+const err_invalid_perms = "Access denied. Invalid user permissions";
+const err_missing_token = "Access denied. No token provided";
+const err_account_not_found = "User account not found. Please contact your admin.";
+const err_unapproved_account = "User account not approved yet. Please contact your admin.";
+
+module.exports = {
+    err_invalid_perms,
+    err_missing_token,
+    err_account_not_found,
+    err_unapproved_account
+}
