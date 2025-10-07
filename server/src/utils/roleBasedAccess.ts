062206470f36e1815489c232c89b968f0ec018cf
robbinat <robbinat@umich.edu>
2025-10-07 16:41:26 -0700

    refactor: separate role defs + application, fix type constraints

--- server/src/utils/roleBasedAccess.ts
+++ server/src/utils/roleBasedAccess.ts
@@ -1,130 +1,96 @@
 import {
 	AbilityBuilder,
 	createMongoAbility,
-	MongoAbility
 } from '@casl/ability';
 
 import { AuthenticatedRequest } from '@/types/auth';
 import { IPermission } from '@/types/models';
-
-// See CASL documentation: https://casl.js.org/v6/en/guide/intro
-
-// Native CASL actions -- CRUD + master action "manage"
-export const ACTIONS = {
-	// Native actions can be used as Mongo query filters with `accessibleBy` function
-	// Note: 'manage' includes ALL actions (native or custom)
-	CASL: {
-		CREATE: 'create',
-		READ: 'read',
-		UPDATE: 'update',
-		DELETE: 'delete',
-		MANAGE: 'manage'
-	},
-	// Custom actions can be used to describe more granular actions instead of just CRUD
-	CUSTOM: { APPROVE: 'approve', PREAPPROVE: 'preapprove' }
-};
-
-// Allowed resources
-// REVIEW: Make names consistent with casl? User action, subject, field, conditions.
-export const RESOURCES = {
-	USER: 'User',
-	SURVEY: 'Survey'
-};
-
-// Allowed scopes
-// REVIEW: Because SCOPES is being used to manage "conditions", maybe we can rename it to CONDITIONS and call it a day. I would also uppercase the values to ALL and SELF.
-export const SCOPES = {
-	ALL: 'all',
-	SELF: 'self'
-};
-
-export const ACTION_ENUM = [
-	...Object.values(ACTIONS.CASL),
-	...Object.values(ACTIONS.CUSTOM)
-];
-export const RESOURCE_ENUM = Object.values(RESOURCES);
-export const SCOPE_ENUM = Object.values(SCOPES);
+import { 
+    ACTIONS, 
+    SUBJECTS, 
+    Ability,
+    Context,
+} from '@/utils/roleDefinitions';
 
 // Assigns authorization by role and action
-// REVIEW: Thoughts on renaming this to defineAbilitiesForUser or similar?
-export default function authorizeUser(
+export default function defineAbilitiesForUser(
 	req: AuthenticatedRequest,
-	userId: string,
+	userObjectId: string,
 	permissions: IPermission[] = []
-): MongoAbility<any> {
-	const { can, cannot, build } = new AbilityBuilder(createMongoAbility);
-	// REVIEW: Thoughts on changing userId to userObjectId everywhere for clarity?
-	const ctx = { id: userId ?? '', employeeId: req.user?.employeeId ?? '' }; // pass in id and employeeId for context
+): Ability {
+    const builder = new AbilityBuilder<Ability>(createMongoAbility);
+	const ctx: Context = { id: userObjectId ?? '', employeeId: req.user?.employeeId ?? '' }; // pass in id and employeeId for context
 
 	// Assign default rules by role
 	switch (req.user?.role) {
 		case 'Admin':
-			adminRules(can);
+			applyAdminPermissions(builder);
 			break;
 		case 'Manager':
-			managerRules(can, ctx);
+			applyManagerPermissions(builder, ctx);
 			break;
 		case 'Volunteer':
-			volunteerRules(can, ctx);
+			applyVolunteerPermissions(builder, ctx);
 			break;
 	}
 
-	// Universal rules that can be overridden by custom rules should go here
+    // Universal rules for all roles that will NOT override custom rules go here
 
-	// Assign custom rules by user-specific permissions
-	customRules(can, ctx, permissions);
+    // Apply custom permissions
+    applyCustomPermissions(builder, ctx, permissions);
 
-	// Universal rules (cannot be overridden by custom rules)
-	cannot(ACTIONS.CUSTOM.APPROVE, RESOURCES.USER, { _id: ctx.id });
+    // Universal rules for all roles that will override any custom rules go here
+    builder.cannot(ACTIONS.CUSTOM.APPROVE, SUBJECTS.USER, { _id: ctx.id });
 
-	return build();
+    return builder.build();
 }
 
-function adminRules(can: any) {
+function applyAdminPermissions(builder: AbilityBuilder<Ability>) {
 	// No restrictions
-	can(ACTIONS.CASL.MANAGE, RESOURCES.USER);
-	can(ACTIONS.CASL.MANAGE, RESOURCES.SURVEY);
+	builder.can(ACTIONS.CASL.MANAGE, SUBJECTS.USER);
+	builder.can(ACTIONS.CASL.MANAGE, SUBJECTS.SURVEY);
 }
 
-function managerRules(can: any, ctx: { id: string; employeeId: string }) {
+function applyManagerPermissions(builder: AbilityBuilder<Ability>, ctx: Context) {
 	// User actions
-	can(ACTIONS.CASL.READ, RESOURCES.USER);
-	can(ACTIONS.CASL.MANAGE, RESOURCES.USER, { employeeId: ctx.employeeId });
-	can(ACTIONS.CASL.MANAGE, RESOURCES.USER, { _id: ctx.id });
-	can(ACTIONS.CUSTOM.APPROVE, RESOURCES.USER);
-	can(ACTIONS.CUSTOM.PREAPPROVE, RESOURCES.USER);
+	builder.can(ACTIONS.CASL.READ, SUBJECTS.USER);
+	builder.can(ACTIONS.CASL.MANAGE, SUBJECTS.USER, { _id: ctx.id });
+	builder.can(ACTIONS.CASL.MANAGE, SUBJECTS.USER, { employeeId: ctx.employeeId });
+	builder.can(ACTIONS.CUSTOM.APPROVE, SUBJECTS.USER);
+	builder.can(ACTIONS.CUSTOM.PREAPPROVE, SUBJECTS.USER);
 
 	// Survey actions
-	can(ACTIONS.CASL.READ, RESOURCES.SURVEY);
-	can(ACTIONS.CASL.CREATE, RESOURCES.SURVEY);
-	can(ACTIONS.CASL.DELETE, RESOURCES.SURVEY, { employeeId: ctx.employeeId });
+	builder.can(ACTIONS.CASL.READ, SUBJECTS.SURVEY);
+	builder.can(ACTIONS.CASL.CREATE, SUBJECTS.SURVEY);
+	builder.can(ACTIONS.CASL.DELETE, SUBJECTS.SURVEY, { employeeId: ctx.employeeId });
 }
 
-function volunteerRules(can: any, ctx: { id: string; employeeId: string }) {
+function applyVolunteerPermissions(builder: AbilityBuilder<Ability>, ctx: Context) {
 	// User actions
-	can(ACTIONS.CASL.READ, RESOURCES.USER, { _id: ctx.id });
-	can(ACTIONS.CASL.READ, RESOURCES.USER, { employeeId: ctx.employeeId });
-	can(ACTIONS.CASL.UPDATE, RESOURCES.USER, { employeeId: ctx.employeeId });
-	can(ACTIONS.CASL.UPDATE, RESOURCES.USER, { _id: ctx.id });
+	builder.can(ACTIONS.CASL.READ, SUBJECTS.USER, { _id: ctx.id });
+	builder.can(ACTIONS.CASL.READ, SUBJECTS.USER, { employeeId: ctx.employeeId });
+	builder.can(ACTIONS.CASL.UPDATE, SUBJECTS.USER, { _id: ctx.id });
+	builder.can(ACTIONS.CASL.UPDATE, SUBJECTS.USER, { employeeId: ctx.employeeId });
 
 	// Survey actions
-	can(ACTIONS.CASL.MANAGE, RESOURCES.SURVEY, { employeeId: ctx.employeeId });
+	builder.can(ACTIONS.CASL.MANAGE, SUBJECTS.SURVEY, { employeeId: ctx.employeeId });
 }
 
-// REVIEW: Thoughts on renaming this to applyCustomPermissions or similar?
-function customRules(
-	can: any,
-	ctx: { id: string; employeeId: string },
+function applyCustomPermissions(
+	builder: AbilityBuilder<Ability>,
+	ctx: Context,
 	permissions: IPermission[]
 ) {
 	permissions.forEach(permission => {
-		if (permission.scope === SCOPES.SELF) {
-			can(permission.action, permission.resource, { _id: ctx.id });
-			can(permission.action, permission.resource, {
-				employeeId: ctx.employeeId
-			});
-		} else {
-			can(permission.action, permission.resource);
-		}
+        if (!permission.condition) {
+            builder.can(permission.action, permission.subject);
+        } else if (permission.condition === 'SCOPE_SELF') {
+            // For SELF condition, create rules for both _id and employeeId
+            builder.can(permission.action, permission.subject, { _id: ctx.id });
+            builder.can(permission.action, permission.subject, { employeeId: ctx.employeeId });
+        } else {
+            // For other conditions, use the condition directly
+            builder.can(permission.action, permission.subject);
+        }
 	});
 }
