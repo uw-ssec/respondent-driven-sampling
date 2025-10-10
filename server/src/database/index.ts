dae45a0834ad0bcbeecdcf241ba11f94d1e18eab
robbinat <robbinat@umich.edu>
2025-10-09 23:19:58 -0700

    chore: create new folder structure with placeholders

---
+++ server/src/database/index.ts
@@ -0,0 +1,12 @@
+// Connection setup
+export { default as connectDB } from './connection/mongoose';
+
+// Type exports
+// export * from './types/user.type';
+// export * from './types/survey.type';
+// export * from './types/seed.type';
+
+// Repository exports
+// export * from './repositories/user.repository';
+// export * from './repositories/survey.repository';
+// export * from './repositories/seed.repository';
