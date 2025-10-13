5a7a17cfe2b3e06ce0794ff9a75fd2798d940daf
robbinat <robbinat@umich.edu>
2025-10-12 23:36:36 -0700

    chore: set up utils

---
+++ server/src/database/utils/constants.ts
@@ -0,0 +1,24 @@
+// ---- SURVEY CONSTANTS ----
+
+export enum SiteLocation {
+    // TODO - get list of locations
+    LOCATION_A = 'Location A',
+}
+
+export const SYSTEM_SURVEY_CODE = '000000';
+export const GENERATED_SURVEY_CODES_LENGTH = 3;
+export const SURVEY_CODE_LENGTH = 6;
+
+// ---- USER CONSTANTS ----
+
+// export enum ApprovalStatus {
+//     PENDING = 'Pending',
+//     APPROVED = 'Approved',
+//     REJECTED = 'Rejected'
+// }
+
+// export enum Role {
+//     VOLUNTEER = 'Volunteer',
+//     MANAGER = 'Manager',
+//     ADMIN = 'Admin'
+// }
