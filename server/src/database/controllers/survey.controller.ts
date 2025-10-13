88bd71aea421055f716b5993938937e7f0ab2f3f
robbinat <robbinat@umich.edu>
2025-10-12 23:38:58 -0700

    feat: implement survey controllers

--- server/src/database/controllers/survey.controller.ts
+++ server/src/database/controllers/survey.controller.ts
@@ -1,1 +1,39 @@
 // TODO: Implement Survey repository
+
+import { create, read, update } from "../utils/operations";
+import { createSurveySchema, updateSurveySchema, readSurveySchema, readSurveysSchema } from "../types/survey.type";
+import { Response } from "express";
+import generateReferralCode from "@/utils/generateReferralCode";
+import { AuthenticatedRequest } from "@/types/auth";
+
+export async function createSurvey(req: AuthenticatedRequest, res: Response) {
+    // Generate 3 unique survey codes for the survey and add to request body
+    req.body.generatedSurveyCodes = Array.from({ length: 3 }, () => ({
+        code: generateReferralCode(),
+        usedBySurveyObjectId: null
+    }));
+
+    const result = await create(req, createSurveySchema);
+    res.status(result.status).json(result);
+}
+
+export async function updateSurvey(req: AuthenticatedRequest, res: Response) {
+    // Insert any request manipulation here
+
+    const result = await update(req, updateSurveySchema);
+    res.status(result.status).json(result);
+}
+
+export async function getSurvey(req: AuthenticatedRequest, res: Response) {
+    // Insert any request manipulation here
+
+    const result = await read(req, readSurveySchema);
+    res.status(result.status).json(result);
+}
+
+export async function getSurveys(req: AuthenticatedRequest, res: Response) {
+    // Insert any request manipulation here
+
+    const result = await read(req, readSurveysSchema);
+    res.status(result.status).json(result);
+}
