8925845647012d62bfc59ac1b23b6fc9d9a29636
Anant Mittal <anmittal@uw.edu>
2025-11-18 15:00:58 -0800

    refactor: remove commented code and delete a file

--- client/src/types/Survey.ts
+++ client/src/types/Survey.ts
@@ -1,15 +1,3 @@
-// import { ReferralCode } from '@/types/ReferralCode';
-
-// export interface Survey {
-// 	referralCodes: ReferralCode[];
-// 	responses: Object;
-// 	_id: string;
-// 	employeeId: string;
-// 	employeeName: string;
-// 	createdAt: string;
-// 	referredByCode?: string;
-// }
-
 import { ISurvey } from '@/database/survey/mongoose/survey.model';
 
 export interface SurveyDocument extends ISurvey {
--- client/src/types/User.ts
+++ client/src/types/User.ts
@@ -1,14 +1,3 @@
-// import { ROLE_ENUM } from '@/permissions/constants';
-
-// export interface User {
-// 	firstName: string;
-// 	lastName: string;
-// 	email: string;
-// 	phone: string;
-// 	role: (typeof ROLE_ENUM)[number];
-// 	[key: string]: any; // For any additional dynamic fields
-// }
-
 import { IUser } from '@/database/user/mongoose/user.model';
 
 export interface UserDocument extends IUser {
